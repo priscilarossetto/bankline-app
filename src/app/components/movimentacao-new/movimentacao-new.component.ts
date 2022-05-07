c29bfdd5965a3638064d4054667a9c57c6ebde4b
Priscila Rossetto <priscilassouzap@gmail.com>
2022-05-07 18:24:22 -0300

    feat: cria rota e componente Nova Movimentação

--- src/app/components/movimentacao-new/movimentacao-new.component.ts
+++ src/app/components/movimentacao-new/movimentacao-new.component.ts
@@ -1,4 +1,8 @@
+// compoenents/movimentacao-new.component.ts
+
 import { Component, OnInit } from '@angular/core';
+import { CorrentistaService } from 'src/app/services/correntista.service';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
 
 @Component({
   selector: 'app-movimentacao-new',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movimentacao-new.component.css']
 })
 export class MovimentacaoNewComponent implements OnInit {
+  correntistas:any;
+  correntista:any;
 
-  constructor() { }
+  constructor(
+    private movimentacaoService: MovimentacaoService,
+    private correntistaService: CorrentistaService,
+    ) { }
 
   ngOnInit(): void {
+    this.exibirCorrentistas();
   }
-
+  exibirCorrentistas(): void {
+    this.correntistaService.list()
+      .subscribe(
+        data => {
+          this.correntistas = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
 }
